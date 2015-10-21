8afd9a8796c96ae0230e57291b76c385faaf77f1
zhiangli <zhiang.luke.li@gmail.com>
2015-10-21 03:04:45 -0400

    add driver destroy, add hitcher and driver edit, redirect some paths like back

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,13 +7,35 @@ class DriversController < ApplicationController
   def new
      @driver = Driver.new
   end
+    
+    def destroy
+        @driver = Driver.find(params[:id])
+        @driver.destroy
+        redirect_to current_user
+    end
+    
+    def edit
+
+        @driver = Driver.find(params[:id])
+        
+    end 
+
+    def update
+        @driver = Driver.find(params[:id])
+        if @driver.update(driver_params)
+            redirect_to current_user
+        else 
+            render "edit"
+        end
+    end   
+
   def create
     
     @driver = current_user.drivers.build(driver_params)
     
-    if  driver.save
+    if  @driver.save
         flash[:success] = "Driver post created!"
-        redirect_to root_url
+        redirect_to current_user
     else
         render "new"
     end    	
