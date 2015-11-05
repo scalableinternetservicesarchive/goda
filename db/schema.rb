34985675ef0f881026b7c24a4d7880250288fa0b
yankaikyan <kyan@umail.ucsb.edu>
2015-11-05 00:17:46 -0500

    add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151105020544) do
 
   create_table "driver_likes", force: :cascade do |t|
