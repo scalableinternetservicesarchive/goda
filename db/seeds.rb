bdd899709e90502f7015dcbc63b9869cc4e3d339
xinliu-UCSB <xinliu@umail.ucsb.edu>
2015-11-25 00:36:25 -0800

    Update seeds.rb
    try things

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 User.delete_all
 Driver.delete_all
 Hitcher.delete_all
+
+User.skip_callback(:create)
+Driver.skip_callback(:create)
+Hitcher.skip_callback(:create)
+
 place1 = ["Santa Barbara, CA, United States", "Los Angeles, CA, United States","San Diego, CA, United States","San Francisco, CA, United States"]
 place2 = ["Arizona, United States", "Utah, United States", "Illinois, United States","San Antonio, TX,  United States"]
 username = "user"
